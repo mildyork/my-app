d70f1fe6d2a0e39b3ce4ef043b0ea4120db2781f
idatagroup\york.zhang <york.zhang@showzapp.com.au>
2015-02-02 11:09:19 +0800

    2/2c

--- js/controller.js
+++ js/controller.js
@@ -1,13 +1,13 @@
 var phonecatApp = angular.module('phonecatApp', []);
 
-phonecatApp.controller('PhoneListCtrl', function($scope){
+phonecatApp.controller('PhoneListCtrl', ['$scope', '$http', function($scope, $http){
 
-	$scope.phones = [
-		{'name': 'Nexus S', 'snippet': 'Fast just got faster with Nexus S.', 'age': 1},
-		{'name': 'Motorola XOOM™ with Wi-Fi', 'snippet': 'The Next, Next Generation tablet.', 'age': 2},
-		{'name': 'MOTOROLA XOOM™ z', 'snippet': 'The Next, Next Generation tablet.', 'age': 3}
-	];
+	$http.get('phones/phones.json').success(function(data){
+		$scope.phones=data;
+
+	});
+
 
 	$scope.orderProp = 'age';
 
-});
+}]);
