d6e6b74e1e09558449854f94725c365cf12e5aa1
York Zhang <yorkcheung@hotmail.com>
2015-02-05 01:30:29 +0800

    2/5

--- js/controller.js
+++ js/controller.js
@@ -12,6 +12,10 @@ phonecatControllers.controller('PhoneListCtrl', ['$scope', '$http', function($sc
 
 }]);
 
-phonecatControllers.controller('PhoneDetailCtrl', ['$scope','$routeParams', function($scope, $routeParams){
-	$scope.phoneId = $routeParams.phoneId;
+phonecatControllers.controller('PhoneDetailCtrl', ['$scope','$routeParams', '$http', function($scope, $routeParams, $http){
+	// $scope.phoneId = $routeParams.phoneId;
+	$http.get('phones/' + $routeParams.phoneId + '.json').success(function(data){
+		$scope.phone = data;
+	});
+
 }]);
