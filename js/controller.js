8c6d307daaa7ff3cb67911415b04302c3d4dbba4
idatagroup\york.zhang <york.zhang@showzapp.com.au>
2015-02-10 11:52:59 +0800

    2/10

--- js/controller.js
+++ js/controller.js
@@ -16,6 +16,14 @@ phonecatControllers.controller('PhoneDetailCtrl', ['$scope','$routeParams', '$ht
 	
 	$http.get('phones/' + $routeParams.phoneId + '.json').success(function(data){
 		$scope.phone = data;
+
+		$scope.mainImageUrl = data.images[0];
 	});
+
+	$scope.setImage = function(imgUrl){
+		$scope.mainImageUrl = imgUrl;
+	}
+
+
 
 }]);
